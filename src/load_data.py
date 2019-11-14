05aca494b10de7bccec46e5df083a21792654951
Fabio Codiglioni <fabiocody@icloud.com>
2019-11-14 13:14:55 +0100

    Second submission test

--- src/load_data.py
+++ src/load_data.py
@@ -7,4 +7,4 @@ from csv import reader
 def load_csv(filename):
     with open(filename) as f:
         csvreader = reader(f, delimiter=',')
-        return [(int(row[0]), int(row[1])) for row in csvreader if csvreader.line_num > 1]     # line_num is used to skip the initial column headers
+        return [row for row in csvreader if csvreader.line_num > 1]     # line_num is used to skip the initial column headers
--- src/old_competition.py
+++ src/old_competition.py
@@ -4,13 +4,14 @@ from load_data import load_csv
 from export_data import export_csv
 from evaluation import evaluate_algorithm
 from random_recommender import RandomRecommender
-from global_effects_recommender import GlobalEffectsRecommender
 import numpy as np
 import scipy.sparse as sps
 
 
 if __name__ == '__main__':
     data = load_csv('old_competition_data/train.csv') + load_csv('old_competition_data/train_sequential.csv')
+    data = [[int(x) for x in line] for line in data]
+    target_playlists = [int(x[0]) for x in load_csv('old_competition_data/target_playlists.csv')]
     playlists = list()
     tracks = list()
     for line in data:
@@ -28,7 +29,7 @@ if __name__ == '__main__':
 
     recommender = RandomRecommender()
     recommender.fit(urm_train)
-    #evaluate_algorithm(urm_test, recommender)
+    evaluate_algorithm(urm_test, recommender)
 
-    data = [(p_id, recommender.recommend(p_id, at=10)) for p_id in range(urm.shape[0])]
+    data = [(p_id, recommender.recommend(p_id, at=10)) for p_id in target_playlists]
     export_csv(data)
