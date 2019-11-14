1966e41094c5add1239c8f9503334fbf7b5b4b8f
Fabio Codiglioni <fabiocody@icloud.com>
2019-11-14 10:25:10 +0100

    Add missing old competition file

--- src/load_data.py
+++ src/load_data.py
@@ -1,1 +1,11 @@
 #!/usr/bin/env python3
+
+
+from csv import reader
+
+
+def load_csv(filename):
+    with open(filename) as f:
+        csvreader = reader(f, delimiter=',')
+        return [row for row in csvreader]
+
