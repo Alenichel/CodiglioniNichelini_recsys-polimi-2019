b6e02e489e2f5f744cec75e98497d9287178d215
Alessandro Nichelini <ale.nichelg@gmail.com>
2019-11-25 17:57:18 +0100

    cooler tuner

--- src/tuner.py
+++ src/tuner.py
@@ -6,6 +6,7 @@ from load_export_csv import load_csv, export_csv
 from evaluation import evaluate_algorithm
 import numpy as np
 import scipy.sparse as sps
+import pprint as pp
 
 class SimilarityFunction(Enum):
     COSINE = "cosine"
@@ -55,12 +56,22 @@ class Tuner:
         return Runner.train_test_loo_split(self.urm)
 
     def run_tuner(self):
-        max_MAP = 0
-        max_param = {
-            'sim_func': "",
-            'top_k_param': '',
-            'shrink_param': ''
+        list_of_parameters = list()
+
+        parameter = {
+            'MAP': 0,
+            'similarity_function': '',
+            'top_k': 0,
+            'shrink': 0
         }
+
+        tuning_results = {
+            'top_MAP': 0,
+            'bottom_MAP': 0,
+            'list': list_of_parameters
+        }
+
+        top_param = dict()
         for sim_func in SimilarityFunction:
             for top_k_param in top_k_parameter_pool:
                 for shrink_param in shrink_parameter_pool:
@@ -71,14 +82,27 @@ class Tuner:
                     recommender.fit(urm_train)
                     print('Evaluating...')
                     round_MAP = evaluate_algorithm(urm_test, recommender)['MAP']
-                    if round_MAP > max_MAP:
-                        max_MAP = round_MAP
-                        max_param['sim_func'] = sim_func.value
-                        max_param['top_k_param'] = top_k_param
-                        max_param['shrink_param'] = shrink_param
+                    round_param = dict()
+                    round_param['MAP'] = round_MAP
+                    round_param['similarity_function'] = sim_func.value
+                    round_param['top_k'] = top_k_param
+                    round_param['shrink'] = shrink_param
 
-        print("Obtained %d with the following parameters %s, %d, %d"
-              % (max_MAP, max_param['sim_func'], max_param['top_k_param'],max_param['shrink_param']))
+                    if round_MAP > tuning_results['top_MAP']:
+                        tuning_results['top_MAP'] = round_MAP
+                        top_param = round_param
+                    list_of_parameters.append(round_param)
+
+        print("Obtained %d with the following parameters %s, %d, %f"
+              % (top_param['MAP'], top_param['similarity_function'], top_param['top_k'], top_param['shrink']))
+
+        list_of_parameters.sort(key=lambda dictionary: dictionary['MAP'], reverse=True)
+        top_ten = list_of_parameters[:10]
+        tuning_results['bottom_MAP'] = top_ten[-1]['MAP']
+
+        print('\nGot also: \n')
+        pp.pprint(tuning_results)
+        return tuning_results
 
     def evaluate(self, top_k, shrink, similarity='cosine', round=10):
         median_value = int()
@@ -94,10 +118,15 @@ class Tuner:
             cumulative_map += evaluate_algorithm(urm_test, recommender)['MAP']
         median_value = cumulative_map / round
         print('The median value of MAP after %d round is: %f' % (round, median_value))
+        return median_value
 
 
 if __name__ == '__main__':
     print('Using Collaborative Filtering (item-based)')
     recommender = ItemCFKNNRecommender()
-    #Tuner().run_tuner()
-    Tuner().evaluate(5, 35)
+    t = Tuner(150)
+    results = t.run_tuner()
+    for parameter in results['list']:
+        parameter['MAP'] = t.evaluate(parameter['top_k'], parameter['shrink'], parameter['similarity_function'])
+    results.sort(key=lambda dictionary: dictionary['MAP'], reverse=True)
+    pp.pprint(results)
