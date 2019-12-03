c0fd42de3246c4f05f268429abefdcdc1c7a83a1
Fabio Codiglioni <fabiocody@icloud.com>
2019-12-03 23:27:21 +0100

    Add LightFMRecommender

--- src/lightfmtest.py
+++ src/lightfmtest.py
@@ -1,8 +1,39 @@
 #!/usr/bin/env python3
 
+import numpy as np
 from lightfm import LightFM
 from lightfm.evaluation import precision_at_k
 from run_utils import build_all_matrices, train_test_split, evaluate, export, SplitType
+
+
+class LightFMRecommender:
+
+    def __init__(self):
+        self.model = LightFM(loss='warp-kos')
+        self.urm = None
+        self.n_users = 0
+        self.n_items = 0
+        self.arange_items = None
+
+    def fit(self, urm, epochs=15):
+        self.urm = urm.tocsr()
+        self.n_users, self.n_items = self.urm.shape
+        self.arange_items = np.arange(self.n_items)
+        self.model.fit(self.urm, epochs=epochs, verbose=True)
+
+    def recommend(self, user_id, at=10, exclude_seen=True):
+        scores = self.model.predict(user_id, self.arange_items)
+        if exclude_seen:
+            scores = self.__filter_seen(user_id, scores)
+        top_items = scores.argsort()[::-1]
+        return top_items[:at]
+
+    def __filter_seen(self, user_id, scores):
+        start_pos = self.urm.indptr[user_id]
+        end_pos = self.urm.indptr[user_id + 1]
+        user_profile = self.urm.indices[start_pos:end_pos]
+        scores[user_profile] = -np.inf
+        return scores
 
 
 EXPORT = False
@@ -12,6 +43,9 @@ if EXPORT:
     urm_test = None
 else:
     urm_train, urm_test = train_test_split(urm, SplitType.LOO)
-model = LightFM(loss='warp-kos')
-model.fit(urm_train, epochs=1, verbose=True)
-print(precision_at_k(model, urm_test, train_interactions=urm_train, k=10).mean())
+lightfm_rec = LightFMRecommender()
+lightfm_rec.fit(urm_train, epochs=15)
+if EXPORT:
+    export(target_users, lightfm_rec)
+else:
+    evaluate(lightfm_rec, urm_test)
