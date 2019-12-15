f495d566b5225922942d6718804bcec87e45aec8
alenichel <ale.nichelg@gmail.com>
2019-12-16 00:27:32 +0100

    hybrid with the best configuration so far

--- src/hybrid.py
+++ src/hybrid.py
@@ -4,10 +4,11 @@ import numpy as np
 from run_utils import build_all_matrices, train_test_split, evaluate, export, SplitType
 from list_merge import round_robin_list_merger, frequency_list_merger, medrank
 from cf import ItemCFKNNRecommender, UserCFKNNRecommender
-from cbf import ItemCBFKNNRecommender
+from cbf import ItemCBFKNNRecommender, UserCBFKNNRecommender
 from cython_modules.SLIM_BPR.SLIM_BPR_CYTHON import SLIM_BPR
 from basic_recommenders import TopPopRecommender
 from enum import Enum
+from slim_elasticnet import SLIMElasticNetRecommender
 
 
 class MergingTechniques(Enum):
@@ -85,6 +86,7 @@ class HybridRecommender:
 
 
 if __name__ == '__main__':
+    #np.random.seed(42)
     EXPORT = False
     urm, icm, ucm, target_users = build_all_matrices()
     if EXPORT:
@@ -94,24 +96,37 @@ if __name__ == '__main__':
         urm_train, urm_test = train_test_split(urm, SplitType.PROBABILISTIC)
     n_users, n_items = urm_train.shape
 
-    tp_rec = TopPopRecommender()
-    tp_rec.fit(urm_train)
+    top_pop = TopPopRecommender()
+    top_pop.fit(urm_train)
 
-    item_cf = ItemCFKNNRecommender(fallback_recommender=tp_rec)
-    item_cf.fit(urm_train, top_k=5, shrink=20, similarity='tanimoto')
+    user_cbf = UserCBFKNNRecommender()
+    user_cbf.fit(urm_train, ucm, top_k=496, shrink=0, normalize=False)
 
-    slim = SLIM_BPR(fallback_recommender=tp_rec)
-    slim.fit(urm_train, epochs=300)
+    hybrid_fb = HybridRecommender([top_pop, user_cbf], urm_train, merging_type=MergingTechniques.MEDRANK)
 
-    user_cf = UserCFKNNRecommender(fallback_recommender=tp_rec)
-    user_cf.fit(urm_train, top_k=715, shrink=60, normalize=True, similarity='tanimoto')
+    item_cf = ItemCFKNNRecommender()
+    item_cf.fit(urm_train, top_k=4, shrink=34, normalize=False, similarity='jaccard')
 
-    cbf = ItemCBFKNNRecommender()
-    cbf.fit(urm_train, icm)
+    user_cf = UserCFKNNRecommender()
+    user_cf.fit(urm_train, top_k=593, shrink=4, normalize=False, similarity='tanimoto')
 
-    hybrid = HybridRecommender([cbf, item_cf, slim, user_cf], merging_type=MergingTechniques.WEIGHTS, weights=[3.048, 4.977, 4.956, 0.025])
+    slim_bpr = SLIM_BPR()
+    slim_bpr.fit(urm_train, epochs=300)
+
+    slim_elasticnet = SLIMElasticNetRecommender()
+    slim_elasticnet.fit(urm_train)
+
+    item_cbf = ItemCBFKNNRecommender()
+    item_cbf.fit(urm_train, icm, 417, 0.3, normalize=True)
+
+    hybrid = HybridRecommender([item_cf, user_cf, slim_bpr, slim_elasticnet, item_cbf],
+                               urm_train,
+                               merging_type=MergingTechniques.WEIGHTS,
+                               weights=[0.9995, 0.08443, 5.576, 0.9982, 0.04314],
+                               fallback_recommender=hybrid_fb
+                               )
 
     if EXPORT:
         export(target_users, hybrid)
     else:
-        evaluate(hybrid, urm_test, cython=True)
+        evaluate(hybrid, urm_test)
