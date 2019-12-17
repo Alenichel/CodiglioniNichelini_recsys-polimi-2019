45e13d1e8a59029a3e03252d7e2803dcb318108a
Alessandro Nichelini <ale.nichelg@gmail.com>
2019-12-17 16:43:45 +0100

    comparison between two hybrids

--- src/user_hybrid.py
+++ src/user_hybrid.py
@@ -7,7 +7,7 @@ from basic_recommenders import TopPopRecommender
 from cbf import ItemCBFKNNRecommender, UserCBFKNNRecommender
 from cf import ItemCFKNNRecommender, UserCFKNNRecommender
 from hybrid import HybridRecommender, MergingTechniques
-from cython_modules.SLIM_BPR.SLIM_BPR_CYTHON import SLIM_BPR as SLIM_BPR_Cython
+from cython_modules.SLIM_BPR.SLIM_BPR_CYTHON import SLIM_BPR
 from slim_elasticnet import SLIMElasticNetRecommender
 
 
@@ -87,28 +87,41 @@ if __name__ == '__main__':
     top_pop = TopPopRecommender()
     top_pop.fit(urm_train)
 
-    '''
-    item_cbf = ItemCBFKNNRecommender()
-    item_cbf.fit(urm_train, icm, top_k=417, shrink=0.3, normalize=True)
     user_cbf = UserCBFKNNRecommender()
-    user_cbf.fit(urm_train, ucm, 500, shrink=0.73, normalize=False)
-    item_cf = ItemCFKNNRecommender()
-    item_cf.fit(urm_train, top_k=45, shrink=40, normalize=True)
-    user_cf = UserCFKNNRecommender()
-    user_cf.fit(urm_train, top_k=500, shrink=2.1, normalize=False)
-    slim_bpr = SLIM_BPR_Cython()
-    slim_bpr.fit(urm_train)
-    slim_elasticnet = SLIMElasticNetRecommender()
-    slim_elasticnet.fit(urm_train)
-    recommenders = [top_pop, item_cbf, user_cbf, item_cf, user_cf, slim_bpr, slim_elasticnet]
-    '''
+    user_cbf.fit(urm_train, ucm, top_k=496, shrink=0, normalize=False)
+
+    hybrid_fb = HybridRecommender([top_pop, user_cbf], urm_train, merging_type=MergingTechniques.MEDRANK)
 
     item_cf = ItemCFKNNRecommender()
-    item_cf.fit(urm_train)
-    slim_bpr = SLIM_BPR_Cython()
-    slim_bpr.fit(urm_train)
-    hybrid = HybridRecommender([item_cf, slim_bpr], urm_train, fallback_recommender=top_pop, merging_type=MergingTechniques.WEIGHTS, weights=[0.5, 0.5])
-    recommenders = [top_pop, item_cf, slim_bpr, hybrid]
+    item_cf.fit(urm_train, top_k=4, shrink=34, normalize=False, similarity='jaccard')
+
+    user_cf = UserCFKNNRecommender()
+    user_cf.fit(urm_train, top_k=593, shrink=4, normalize=False, similarity='tanimoto')
+
+    slim_bpr = SLIM_BPR()
+    slim_bpr.fit(urm_train, epochs=300)
+
+    slim_elasticnet = SLIMElasticNetRecommender()
+    slim_elasticnet.fit(urm_train)
+
+    item_cbf = ItemCBFKNNRecommender()
+    item_cbf.fit(urm_train, icm, 417, 0.3, normalize=True)
+
+    hybrid1 = HybridRecommender([item_cf, user_cf, slim_bpr, slim_elasticnet, item_cbf],
+                               urm_train,
+                               merging_type=MergingTechniques.WEIGHTS,
+                               weights=[0.9995, 0.08443, 5.576, 0.9982, 0.04314],
+                               fallback_recommender=hybrid_fb
+                               )
+
+    hybrid2 = HybridRecommender([item_cf, user_cf, slim_bpr, slim_elasticnet, item_cbf],
+                               urm_train,
+                               merging_type=MergingTechniques.WEIGHTS,
+                               weights=[2.944, 0.1427, 1.12, 2.958, 0.005846],
+                               fallback_recommender=hybrid_fb
+                               )
+
+    recommenders = [hybrid1, hybrid2]
 
     user_segmenter = UserSegmenter(recommenders, urm_train, urm_test)
     user_segmenter.analyze(group_size_percent=0.05)
