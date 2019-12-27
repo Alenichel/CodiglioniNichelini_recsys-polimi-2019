93ff491939e8d9c481bed48b5d7bd68dc24503ee
Alessandro Nichelini <ale.nichelg@gmail.com>
2019-12-27 15:31:26 +0100

    added als

--- src/mf.py
+++ src/mf.py
@@ -2,6 +2,7 @@
 
 import numpy as np
 import torch
+import implicit
 from torch.utils.data import Dataset, DataLoader
 from torch.autograd import Variable
 from run_utils import build_all_matrices, train_test_split, SplitType, export, evaluate
@@ -104,14 +105,67 @@ class MFRecommender:
         return ranking[:at]
 
 
+class AlternatingLeastSquare:
+    """
+    ALS implemented with implicit following guideline of
+    https://medium.com/radon-dev/als-implicit-collaborative-filtering-5ed653ba39fe
+    IDEA:
+    Recomputing x_{u} and y_i can be done with Stochastic Gradient Descent, but this is a non-convex optimization problem.
+    We can convert it into a set of quadratic problems, by keeping either x_u or y_i fixed while optimizing the other.
+    In that case, we can iteratively solve x and y by alternating between them until the algorithm converges.
+    This is Alternating Least Squares.
+    """
+
+    def __init__(self, n_factors=300, regularization=0.15, iterations=30):
+        self.n_factors = n_factors
+        self.regularization = regularization
+        self.iterations = iterations
+
+    def fit(self, URM):
+        self.URM = URM
+        sparse_item_user = self.URM.T
+
+        # Initialize the als model and fit it using the sparse item-user matrix
+        model = implicit.als.AlternatingLeastSquares(factors=self.n_factors, regularization=self.regularization, iterations=self.iterations)
+
+        alpha_val = 24
+        # Calculate the confidence by multiplying it by our alpha value.
+
+        data_conf = (sparse_item_user * alpha_val).astype('double')
+
+        # Fit the model
+        model.fit(data_conf)
+
+        # Get the user and item vectors from our trained model
+        self.user_factors = model.user_factors
+        self.item_factors = model.item_factors
+
+    def get_expected_ratings(self, playlist_id):
+        scores = np.dot(self.user_factors[playlist_id], self.item_factors.T)
+
+        return np.squeeze(scores)
+
+    def recommend(self, user_id, at=10):
+        user_id = int(user_id)
+        expected_ratings = self.get_expected_ratings(user_id)
+
+        recommended_items = np.flip(np.argsort(expected_ratings), 0)
+
+        unseen_items_mask = np.in1d(recommended_items, self.URM[user_id].indices,
+                                    assume_unique=True, invert=True)
+        recommended_items = recommended_items[unseen_items_mask]
+        return recommended_items[0:at]
+
+
 if __name__ == '__main__':
+    np.random.seed(42)
     EXPORT = False
-    urm, icm, target_users = build_all_matrices()
+    urm, icm, ucm, target_users = build_all_matrices()
     if EXPORT:
         urm_train = urm.tocsr()
         urm_test = None
     else:
-        urm_train, urm_test = train_test_split(urm, SplitType.LOO_CYTHON)
+        urm_train, urm_test = train_test_split(urm, SplitType.PROBABILISTIC)
     use_cuda = False
     if use_cuda and torch.cuda.is_available():
         device = torch.device('cuda')
@@ -119,7 +173,7 @@ if __name__ == '__main__':
     else:
         device = torch.device('cpu')
         print("MF_MSE_PyTorch: Using CPU")
-    rec = MFRecommender()
+    rec = AlternatingLeastSquare()
     rec.fit(urm_train)
     if EXPORT:
         export(target_users, rec)
