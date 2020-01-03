0338762eb4e0b47c89a2e4498478292394b54ac4
alenichel <ale.nichelg@gmail.com>
2020-01-03 13:49:51 +0100

    fixed clusterization top pop

--- src/clusterization.py
+++ src/clusterization.py
@@ -7,6 +7,24 @@ from sklearn.cluster import KMeans
 from run_utils import DataFiles
 
 
+def get_clusters():
+    np.random.seed(42)
+    age_data = pd.read_csv(DataFiles.UCM_AGE)
+    age_data = age_data.drop(['data'], axis=1)
+    age_data = age_data.rename(columns={'row': 'user_id', 'col': 'age'})
+    region_data = pd.read_csv(DataFiles.UCM_REGION)
+    region_data = region_data.drop(['data'], axis=1)
+    region_data = region_data.rename(columns={'row': 'user_id', 'col': 'region'})
+    data = pd.merge(age_data, region_data, on='user_id')
+    X = data.drop(['user_id'], axis=1)
+    kmeans = KMeans(n_clusters=10, init='k-means++', max_iter=300, n_init=10, random_state=42)
+    pred_y = kmeans.fit_predict(X)
+    clusters = {cluster: list() for cluster in range(max(pred_y) + 1)}
+    for i in range(len(data)):
+        cluster_id = pred_y[i]
+        user_id = data.user_id.iloc[i]
+        clusters[cluster_id].append(user_id)
+    return clusters
 if __name__ == '__main__':
     np.random.seed(42)
     age_data = pd.read_csv(DataFiles.UCM_AGE)
--- src/clusterized_top_pop.py
+++ src/clusterized_top_pop.py
@@ -5,6 +5,7 @@ from tqdm import trange
 from run_utils import build_all_matrices, clusterize, train_test_split, SplitType, export, evaluate
 from basic_recommenders import TopPopRecommender
 from cbf import UserCBFKNNRecommender
+from clusterization import get_clusters
 
 
 class ClusterizedTopPop:
@@ -15,11 +16,15 @@ class ClusterizedTopPop:
         self.recommenders = dict()
         self.cluster_for_user = dict()
         self.std_top_pop = None
+        self.clusters = None
+        self.std_top_pop = None
 
-    def fit(self, urm_train):
+    def fit(self, urm_train, clusters):
         self.urm_train = urm_train
-        clusters = clusterize()
-        for cluster in clusters:
+        self.clusters = clusters
+        self.std_top_pop = TopPopRecommender()
+        self.std_top_pop.fit(urm_train)
+        for cluster in self.clusters:
             users = clusters[cluster]
             for user_id in users:
                 self.cluster_for_user[user_id] = cluster
@@ -29,8 +34,11 @@ class ClusterizedTopPop:
             self.recommenders[cluster] = top_pop
 
     def recommend(self, user_id, at=None, exclude_seen=True):
-        cluster = self.cluster_for_user[user_id]
-        return self.recommenders[cluster].recommend(user_id, at, False)
+        try:
+            cluster = self.cluster_for_user[user_id]
+            return self.recommenders[cluster].recommend(user_id, at, False)
+        except KeyError:
+            return self.std_top_pop.recommend(user_id, at, exclude_seen)
 
 
 if __name__ == '__main__':
@@ -43,8 +51,9 @@ if __name__ == '__main__':
     else:
         urm_train, urm_test = train_test_split(urm, SplitType.PROBABILISTIC)
     # TOP-POP
+    clusters = get_clusters()
     top_pop = ClusterizedTopPop()
-    top_pop.fit(urm_train)
+    top_pop.fit(urm_train, clusters)
     # USER CBF
     user_cbf = UserCBFKNNRecommender()
     user_cbf.fit(urm_train, ucm, top_k=496, shrink=0, normalize=False)
