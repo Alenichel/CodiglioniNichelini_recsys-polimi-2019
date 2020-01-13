7799e8fdbb6be65920bd4b1cc819bac826257bab
alenichel <ale.nichelg@gmail.com>
2020-01-13 20:57:07 +0100

    using sklearn

--- src/lgbm_new_new.py
+++ src/lgbm_new_new.py
@@ -4,16 +4,24 @@ import os
 import numpy as np
 import pandas as pd
 import lightgbm as lgb
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.model_selection import GridSearchCV
 from tqdm import trange
-from run_utils import set_seed, build_urm, train_test_split, build_age_ucm, build_region_ucm, get_seed, evaluate
 
+from sklearn import metrics
+from run_utils import set_seed, build_urm, train_test_split, build_age_ucm, build_region_ucm, get_seed, evaluate, \
+    build_all_matrices
+import matplotlib.pylab as plt
+from matplotlib.pylab import rcParams
 
 class LGBMRecommender:
 
-    def __init__(self, urm_train, ucm_age, ucm_region):
+    def __init__(self, urm_train, ucm_age, ucm_region, min=None, max=None):
         self.urm_train = urm_train.tocsr()
         self.n_users, self.n_items = urm_train.shape
         self.profile_length = np.ediff1d(urm_train.tocsr().indptr)
+        self.min = min if min else 0
+        self.max = max if max else self.n_items
         cache_dir = 'models/lgbm/'
         cache_filename = cache_dir + self.get_cache_filename() + '.csv'
         if not os.path.exists(cache_filename):
@@ -27,21 +35,11 @@ class LGBMRecommender:
             self.df.to_csv(cache_filename)
         else:
             self.df = pd.read_csv(cache_filename)
-            self.df = self.df.drop(['Unnamed: 0'], axis=1)
+            #self.df = self.df.drop(['Unnamed: 0'], axis=1)
         self.params = {
-            'learning_rate': 0.01,
-            'boosting_type': 'gbdt',
-            'objective': 'binary',
-            # 'metric': 'binary_logloss',
-            # 'sub_feature': 0.5,
-            # 'num_leaves': 100,
-            # 'min_data': 50,
-            # 'max_depth': 10,
-            'verbose': -1,
-            # 'num_thread': 6,
-            # 'device': 'cpu',
-            # 'max_bin': 200,
-            # 'gpu_use_dp': False
+            'learning_rate': 0.1,
+            'max_depth': 0.1,
+            'random_state': 42
         }
         self.y_pred = np.zeros(urm_train.shape, dtype=float)
 
@@ -59,18 +57,29 @@ class LGBMRecommender:
         return values[length - 1] if length > 0 else 42424242
 
     def fit(self):
-        feature_names = [c for c in self.df.columns if not c.endswith('_id')]
-        cat_features = [c for c in self.df.columns if not c.endswith('_id')]
-        for item_id in trange(self.n_items):
-            y = self.urm_train[:, item_id].toarray().ravel()
-            train_dataset = lgb.Dataset(self.df,
-                                        label=y,
-                                        feature_name=feature_names,
-                                        categorical_feature=cat_features,
-                                        free_raw_data=False)
-            model = lgb.train(self.params, train_set=train_dataset, feature_name=feature_names, categorical_feature=cat_features)
-            preds = model.predict(self.df)
-            self.y_pred[:, item_id] = preds
+        counter = 0
+        for item_id in trange(self.max):
+            if counter < self.min:
+                counter += 1
+                continue
+            if counter == self.max:
+                break
+            target_column = self.urm_train[:, item_id].toarray().ravel()
+            if max(target_column) == 0:
+                continue
+            round_df = self.df
+            round_df['Target'] = target_column
+            predictors = [x for x in round_df.columns if x not in ['Unnamed: 0', 'Target']]
+            gbm = GradientBoostingClassifier(learning_rate=0.1,
+                                             max_depth=1,
+                                             random_state=42)
+            gbm.fit(round_df[predictors], round_df['Target'])
+            # Predict training set:
+            #dtrain_predictions = gbm.predict(round_df[predictors])
+            dtrain_predprob = gbm.predict_proba(round_df[predictors])[:, 1]
+            self.y_pred[:, item_id] = dtrain_predprob
+            counter += 1
+
 
     def recommend(self, user_id, at=None, exclude_seen=True):
         user_profile = self.y_pred[user_id]
@@ -89,13 +98,14 @@ class LGBMRecommender:
 
 if __name__ == '__main__':
     set_seed(42)
-    urm = build_urm()
+    rcParams['figure.figsize'] = 12, 4
+    urm, icm, ucm, target_users = build_all_matrices()
     urm_train, urm_test = train_test_split(urm)
     urm_train = urm_train.tocoo()
     ucm_age = build_age_ucm(urm_train.shape[0])
     ucm_region = build_region_ucm(urm_train.shape[0])
 
-    rec = LGBMRecommender(urm_train, ucm_age, ucm_region)
+    rec = LGBMRecommender(urm_train, ucm_age, ucm_region, min=3000, max=4000)
     rec.fit()
 
     evaluate(rec, urm_test)
